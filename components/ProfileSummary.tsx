a0431972457b5a7fdd81275418688621ba96967c
kidesleo <kidus.dev@gmail.com>
2022-12-26 17:38:06 +0300

    Added profile summary component

---
+++ components/ProfileSummary.tsx
@@ -0,0 +1,49 @@
+import { Profile } from "../constants/types";
+import * as changeCase from "change-case";
+
+export default function ProfileSummary({
+    userProfile,
+}: {
+    userProfile: Profile;
+}) {
+    const ListInfo = Object.keys(userProfile).map((key) => {
+        if (
+            key === "education" ||
+            key === "experience" ||
+            key === "yearsOfExp" ||
+            key === "skills"
+        )
+            return;
+
+        let label: string = changeCase.sentenceCase(key);
+        const value: string = userProfile[key as keyof Profile].toString();
+        if (key === "prefLocation") label = "Preferred Location";
+
+        return (
+            <div className="my-2" key={key}>
+                <label
+                    htmlFor={key}
+                    className="font-medium mb-2 leading-none inline-block"
+                >
+                    {label}
+                </label>
+                <label
+                    id={key}
+                    className="w-auto mx-4 rounded-full border-slate-300"
+                >
+                    {value}
+                </label>
+            </div>
+        );
+    });
+    return (
+        <div className="container">
+            <div className="w-full max-w-[1000px] mx-auto my-10 bg-white shadow-normal border border-slate-700 rounded-[25px] p-8 md:py-14 md:px-20 flex flex-col justify-center items-center">
+                <h2 className="font-semibold text-lg md:text-3xl mb-4">
+                    Profile Summary
+                </h2>
+                {ListInfo}
+            </div>
+        </div>
+    );
+}
