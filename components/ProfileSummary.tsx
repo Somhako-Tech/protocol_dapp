cf51ac7e725d6d4c877cea2d28addc6b114e2407
kidesleo <kidus.dev@gmail.com>
2022-12-27 18:58:16 +0300

    Refactored profile summary

--- components/ProfileSummary.tsx
+++ components/ProfileSummary.tsx
@@ -1,38 +1,59 @@
+import { useEffect, useState } from "react";
 import { Profile } from "../constants/types";
 import * as changeCase from "change-case";
+import { axiosContractInstance } from "../constants/axiosInstances";
 
 export default function ProfileSummary({
-    userProfile,
+    handle,
 }: {
-    userProfile: Profile;
+    handle: string | string[] | undefined;
 }) {
-    const ListInfo = Object.keys(userProfile).map((key) => {
-        if (
-            key === "education" ||
-            key === "experience" ||
-            key === "yearsOfExp" ||
-            key === "skills"
-        )
-            return;
+    const [userProfile, setUserProfile] = useState<Profile>();
+    useEffect(() => {
+        if (handle === "" || !handle) return;
+        const getProfile = async () =>
+            await axiosContractInstance
+                .get(`/profile/${handle}`)
+                .then((res) => {
+                    setUserProfile(res.data.profile);
+                });
+        getProfile();
+    }, [handle]);
 
-        let label: string = changeCase.sentenceCase(key);
-        const value: string = userProfile[key as keyof Profile].toString();
-        if (key === "prefLocation") label = "Preferred Location";
+    const ListInfo = userProfile ? (
+        Object.keys(userProfile).map((key) => {
+            if (
+                key === "education" ||
+                key === "experience" ||
+                key === "yearsOfExp" ||
+                key === "skills"
+            )
+                return;
 
-        return (
-            <div className="my-2" key={key}>
-                <label
-                    htmlFor={key}
-                    className="font-medium text-base mb-2 leading-none inline-block"
-                >
-                    {label}
-                </label>
-                <label id={key} className="font-medium text-base w-auto mx-4 ">
-                    {value}
-                </label>
-            </div>
-        );
-    });
+            let label: string = changeCase.sentenceCase(key);
+            const value: string = userProfile[key as keyof Profile].toString();
+            if (key === "prefLocation") label = "Preferred Location";
+
+            return (
+                <div className="my-2" key={key}>
+                    <label
+                        htmlFor={key}
+                        className="font-medium text-base mb-2 leading-none inline-block"
+                    >
+                        {label}
+                    </label>
+                    <label
+                        id={key}
+                        className="font-medium text-base w-auto mx-4 "
+                    >
+                        {value}
+                    </label>
+                </div>
+            );
+        })
+    ) : (
+        <></>
+    );
     return (
         <div className="container">
             <div className="w-full max-w-[1000px] mx-auto my-10 bg-white shadow-normal border border-slate-700 rounded-[25px] p-8 md:py-14 md:px-20 flex flex-col justify-center items-center">
