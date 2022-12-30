f6dcca29c7aca41a09105b9fa801899de464efae
kidesleo <kidus.dev@gmail.com>
2022-12-30 14:06:36 +0300

    Refactored sign in page to join

--- pages/join/index.tsx
+++ pages/join/index.tsx
@@ -1,61 +1,52 @@
 import { getCsrfToken, getProviders, signIn } from "next-auth/react";
+import { axiosAPIInstance } from "../../constants/axiosInstances";
+import { useReferralStore } from "../../store";
+import { useRouter } from "next/router";
 
 export default function LogIn({ providers }: { providers: any }) {
+    const router = useRouter();
+
+    const { referral } = router.query;
+    const [setReferredFrom, referredFrom] = useReferralStore((state) => [
+        state.setReferredFrom,
+        state.referredFrom,
+    ]);
+
+    //TODO Add check for users that don't exist
+    const handleReferralSignIn = async (provider_id: any) => {
+        if (referral && typeof referral === "string") {
+            await axiosAPIInstance(`/profile/${referral}`).then((response) => {
+                const referredFrom = response.data.profile.user_id;
+                setReferredFrom(referredFrom);
+                signIn(provider_id);
+            });
+        } else signIn(provider_id);
+    };
+
     return (
         <div className="relative flex items-top justify-center min-h-screen bg-white dark:bg-gray-900 sm:items-center sm:pt-0">
             <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div className="mt-8 overflow-hidden">
                     <div className="grid grid-cols-1 md:grid-cols-2">
                         <div className="p-6 mr-2 bg-gray-100 dark:bg-gray-800 sm:rounded-lg">
-                            <h1 className="text-4xl sm:text-5xl text-gray-800 dark:text-white font-extrabold tracking-tight">
-                                Get in touch
-                            </h1>
                             <p className="text-normal text-lg sm:text-2xl font-medium text-gray-600 dark:text-gray-400 mt-2">
-                                Fill in the form to start a conversation
+                                Sign in to mint your account
                             </p>
 
-                            <div className="flex items-center mt-8 text-gray-600 dark:text-gray-400">
-                                <svg
-                                    fill="none"
-                                    stroke="currentColor"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="1.5"
-                                    viewBox="0 0 24 24"
-                                    className="w-8 h-8 text-gray-500"
-                                >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="1.5"
-                                        d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                                    />
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="1.5"
-                                        d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                                    />
-                                </svg>
-                                <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                                    Acme Inc, Street, State, Postal Code
-                                </div>
-                            </div>
+                            <div className="flex items-center mt-8 text-gray-600 dark:text-gray-400"></div>
 
                             <div className="flex items-center mt-4 text-gray-600 dark:text-gray-400">
                                 <svg
                                     fill="none"
                                     stroke="currentColor"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="1.5"
+                                    strokeLinejoin="round"
+                                    strokeWidth="1.5"
                                     viewBox="0 0 24 24"
                                     className="w-8 h-8 text-gray-500"
                                 >
                                     <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="1.5"
+                                        strokeLinejoin="round"
+                                        strokeWidth="1.5"
                                         d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                                     />
                                 </svg>
@@ -68,16 +59,14 @@ export default function LogIn({ providers }: { providers: any }) {
                                 <svg
                                     fill="none"
                                     stroke="currentColor"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="1.5"
+                                    strokeLinejoin="round"
+                                    strokeWidth="1.5"
                                     viewBox="0 0 24 24"
                                     className="w-8 h-8 text-gray-500"
                                 >
                                     <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="1.5"
+                                        strokeLinejoin="round"
+                                        strokeWidth="1.5"
                                         d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                                     />
                                 </svg>
@@ -95,21 +84,25 @@ export default function LogIn({ providers }: { providers: any }) {
                                     Please sign in to continue:
                                 </p>
                                 {providers &&
-                                    Object.values(providers).map((provider) => (
-                                        <div
-                                            key={provider.name}
-                                            style={{ marginBottom: 0 }}
-                                        >
-                                            <button
-                                                className="w-full max-w-sm rounded-full bg-blue-500 text-white font-bold py-2 px-4 hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-                                                onClick={() =>
-                                                    signIn(provider.id)
-                                                }
+                                    Object.values(providers).map(
+                                        (provider: any) => (
+                                            <div
+                                                key={provider.name}
+                                                style={{ marginBottom: 0 }}
                                             >
-                                                Sign in with {provider.name}
-                                            </button>
-                                        </div>
-                                    ))}
+                                                <button
+                                                    className="w-full max-w-sm rounded-full bg-blue-500 text-white font-bold py-2 px-4 hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+                                                    onClick={() =>
+                                                        handleReferralSignIn(
+                                                            provider.id
+                                                        )
+                                                    }
+                                                >
+                                                    Sign in with {provider.name}
+                                                </button>
+                                            </div>
+                                        )
+                                    )}
                             </div>
                         </div>
                     </div>
@@ -119,7 +112,7 @@ export default function LogIn({ providers }: { providers: any }) {
     );
 }
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps(context: any) {
     const providers = await getProviders();
     const csrfToken = await getCsrfToken(context);
     return {
