8c6eaab92867b22a28b9719b4b3f958438b156f8
kidesleo <kidus.dev@gmail.com>
2023-01-02 15:11:36 +0300

    Updated user page

--- pages/u/[handle].tsx
+++ pages/u/[handle].tsx
@@ -7,17 +7,34 @@ import "ethers";
 import React from "react";
 import { Profile } from "../../constants/types";
 
-import { axiosContractInstance } from "../../constants/axiosInstances";
-import InputBox from "../../components/InputBox";
-import SignInWith from "../../components/SignInWith";
+import { axiosAPIInstance } from "../../constants/axiosInstances";
 import { useRouter } from "next/router";
 import ProfileSummary from "../../components/ProfileSummary";
 import { useMintStore } from "../../store";
 import Header from "../../components/Header";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import Snackbar from "@mui/material/Snackbar";
+import { useQuery } from "@tanstack/react-query";
+import { useSession } from "next-auth/react";
 
 export default function UserPage() {
     const router = useRouter();
     const { handle } = router.query;
+    const { data: session } = useSession();
+
+    const getReferralCount = async (userId: number) => {
+        const { data } = await axiosAPIInstance.get(
+            `/referrals?user_id=${userId}`
+        );
+        return data;
+    };
+
+    const { data, isLoading, isError } = useQuery(
+        ["referral", session?.user?.id],
+        () => getReferralCount(session?.user?.id ? session.user.id : 0)
+    );
+
+    const referralCount = isLoading ? 0 : isError ? 0 : data.count;
 
     useEffect(() => {
         if (!handle || typeof handle !== "string") router.push("/app");
@@ -25,9 +42,43 @@ export default function UserPage() {
 
     const [tokenId] = useMintStore((state) => [state.tokenId]);
 
+    const [isCopied, setIsCopied] = useState(false);
+
+    async function copyTextToClipboard(text: string) {
+        if ("clipboard" in navigator) {
+            return await navigator.clipboard.writeText(text);
+        } else {
+            return document.execCommand("copy", true, text);
+        }
+    }
+
+    const referralLink = `http://localhost:3000/join?referral=${handle}`;
+
+    // onClick handler function for the copy button
+    const handleCopyClick = () => {
+        // Asynchronously call copyTextToClipboard
+        copyTextToClipboard(referralLink)
+            .then(() => {
+                // If successful, update the isCopied state value
+                setIsCopied(true);
+                setTimeout(() => {
+                    setIsCopied(false);
+                }, 1500);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
     return (
         <main className="py-8 bg-white">
-            <Header />
+            {/* inMintQueue should depend on Query state */}
+            <Header inMintQueue={true} />
+            <Snackbar
+                open={isCopied}
+                autoHideDuration={6000}
+                message="Link copied"
+            />
             <section className="w-full flex flex-wrap ">
                 <div className="container h-full">
                     <div className="w-full max-w-[800px] mx-auto text-black	 bg-white shadow-normal  rounded-[25px] p-8 md:py-14 md:px-20">
@@ -40,12 +91,39 @@ export default function UserPage() {
                                 Minting Complete! Your token id is{" "}
                                 {`'${tokenId}'`}!
                                 <ProfileSummary handle={handle} />
-                                <button
-                                    onClick={() => router.push("/home")}
-                                    className=" bg-gradient-to-r from-[#6D27F9] to-[#9F09FB] text-white font-bold rounded-full py-2.5 px-6 md:min-w-[150px] transition-all hover:from-[#391188] hover:to-[#391188]"
-                                >
-                                    Home
-                                </button>
+                                <div className="w-full max-w-[1000px] mx-auto my-10 bg-white shadow-normal border border-slate-700 rounded-[25px] p-8 md:py-14 md:px-20 flex flex-col justify-center items-center">
+                                    <label
+                                        htmlFor="referral"
+                                        className="font-medium text-base mb-2 leading-none inline-block"
+                                    >
+                                        Referral Link
+                                    </label>
+                                    <div>
+                                        <label
+                                            id="referral"
+                                            className="font-medium text-base w-auto mx-4 text-somhakohr2"
+                                        >
+                                            {referralLink}{" "}
+                                        </label>
+                                        <ContentCopyIcon
+                                            onClick={handleCopyClick}
+                                        />
+                                    </div>
+                                    <div>
+                                        <label
+                                            id="referral"
+                                            className="font-medium text-base w-auto mx-4 "
+                                        >
+                                            Referred Accounts :
+                                        </label>
+                                        <label
+                                            id="referral"
+                                            className="font-medium text-base w-auto mx-4 text-somhakohr2"
+                                        >
+                                            {referralCount}{" "}
+                                        </label>
+                                    </div>
+                                </div>
                             </h1>
                         </div>
                     </div>
