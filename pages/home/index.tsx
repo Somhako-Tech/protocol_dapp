b1e1371352724f5d430ea5b90f6c544bd9a9d23b
kidesleo <kidus.dev@gmail.com>
2023-01-05 20:03:07 +0300

    Switched from react query to graphql query

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -2,50 +2,50 @@ import { useEffect, useState } from "react";
 import "@rainbow-me/rainbowkit/styles.css";
 import "ethers";
 // import { axiosInstance } from "../api/axiosApi";
-import { Profile } from "../../constants/types";
 
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import Header from "../../components/Header";
 import { useQuery } from "@tanstack/react-query";
-import ProfilePreview from "../../components/ProfilePreview";
-import { ProfileFormSkeleton } from "../../components/skeletons";
-import { getProfileById, getUserById } from "../../constants/axiosInstances";
+import {
+    getProfileByUserIdQuery,
+    getUserQuery,
+} from "../../graphql/graphqlQueries";
 
 export default function Home() {
     const router = useRouter();
     const { data: session } = useSession();
 
-    const {
-        isLoading: isProfileListLoading,
-        isError: profileListError,
-        data: ProfileList,
-    } = useQuery({
-        queryKey: ["profiles"],
-        queryFn: () =>
-            fetch("http://localhost:3030/profile").then((res) => res.json()),
-    });
+    // const {
+    //     isLoading: isProfileListLoading,
+    //     isError: profileListError,
+    //     data: ProfileList,
+    // } = useQuery({
+    //     queryKey: ["profiles"],
+    //     queryFn: () =>
+    //         fetch("http://localhost:3030/profile").then((res) => res.json()),
+    // });
 
-    const { isLoading: isProfileQueryLoading, data: Profile } = useQuery(
-        ["profileById", session?.user?.id],
-        () => getProfileById(session?.user?.id ? session.user.id : 0),
-        { enabled: !!session }
+    const {
+        isLoading: isProfileQueryLoading,
+        isError: isProfileQueryError,
+        data: Profile,
+    } = useQuery(["getProfile", session?.user.id], async () =>
+        getProfileByUserIdQuery(session?.user.id || "default")
     );
 
     const { isLoading: isUserQueryLoading, data: User } = useQuery(
-        ["userById", session?.user?.id],
-        () => getUserById(session?.user?.id ? session.user.id : 0),
-        { enabled: !!session }
+        ["getUser", session?.user.id],
+        async () => getUserQuery(session?.user.id || "default")
     );
 
     const [inMintQueue, setInMintQueue] = useState(false);
 
     //TODO turn this into a middleware
     useEffect(() => {
-        if (!isProfileQueryLoading && !isUserQueryLoading) {
-            console.log(User);
-            if (User.data.is_admin) router.push("/admin");
-            if (Profile.error && !User.data.is_admin) {
+        if (!isProfileQueryLoading && !isUserQueryLoading && User) {
+            console.log({ User, Profile });
+            if (User.is_admin) router.push("/admin");
+            if (!Profile && !User.is_admin) {
                 router.push("/app");
             } else setInMintQueue(true);
         }
@@ -53,45 +53,46 @@ export default function Home() {
         Profile,
         User,
         inMintQueue,
+        isProfileQueryError,
         isProfileQueryLoading,
         isUserQueryLoading,
         router,
     ]);
 
-    useEffect(() => {
-        if (!session) router.push("/");
-    }, [session, router]);
+    // useEffect(() => {
+    //     if (!session) router.push("/");
+    // }, [session, router]);
 
-    if (isProfileListLoading) {
-        return (
-            <section className="w-full flex flex-wrap ">
-                <div className="container h-full">
-                    <div className="w-full mx-auto text-black	 bg-white shadow-normal  rounded-[25px] p-8 md:py-14 md:px-20">
-                        <ProfileFormSkeleton />
-                    </div>
-                </div>
-            </section>
-        );
-    }
+    // if (isProfileListLoading) {
+    //     return (
+    //         <section className="w-full flex flex-wrap ">
+    //             <div className="container h-full">
+    //                 <div className="w-full mx-auto text-black	 bg-white shadow-normal  rounded-[25px] p-8 md:py-14 md:px-20">
+    //                     <ProfileFormSkeleton />
+    //                 </div>
+    //             </div>
+    //         </section>
+    //     );
+    // }
     let queuedProfileList = <></>;
     let mintedProfileList = <></>;
 
-    if (!isProfileListLoading && !profileListError) {
-        mintedProfileList = ProfileList.profiles.map((profile: any) => {
-            if (profile.minted)
-                return (
-                    <ProfilePreview profile={profile} key={profile.handle} />
-                );
-            else return;
-        });
-        queuedProfileList = ProfileList.profiles.map((profile: any) => {
-            if (profile.minted) return;
-            else
-                return (
-                    <ProfilePreview profile={profile} key={profile.handle} />
-                );
-        });
-    }
+    // if (!isProfileListLoading && !profileListError) {
+    //     mintedProfileList = ProfileList.profiles.map((profile: any) => {
+    //         if (profile.minted)
+    //             return (
+    //                 <ProfilePreview profile={profile} key={profile.handle} />
+    //             );
+    //         else return;
+    //     });
+    //     queuedProfileList = ProfileList.profiles.map((profile: any) => {
+    //         if (profile.minted) return;
+    //         else
+    //             return (
+    //                 <ProfilePreview profile={profile} key={profile.handle} />
+    //             );
+    //     });
+    // }
 
     return (
         <section className="w-full flex flex-wrap ">
