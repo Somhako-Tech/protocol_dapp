9a7e48bda16cc766b4c98b564dce26dc78a2f36a
kidesleo <kidus.dev@gmail.com>
2023-01-06 10:51:35 +0300

    Moved mutations to mutations file

--- graphql/graphqlQueries.tsx
+++ graphql/graphqlQueries.tsx
@@ -7,15 +7,6 @@ import { Prisma, Profile } from "@prisma/client";
 
 const API_URL = `http://localhost:3030/graphql`;
 
-export const getProfileByUserIdQueryDocument = graphql(`
-    query getProfileById($user_id: String!) {
-        getProfile(where: { user_id: $user_id }) {
-            minted
-            handle
-        }
-    }
-`);
-
 export const getProfileByHandleQueryDocument = graphql(`
     query getProfileByHandle($handle: String!) {
         getProfile(where: { handle: $handle }) {
@@ -37,6 +28,15 @@ export const getProfileByHandleIdQuery = async (handle: string) => {
 
     return getProfile;
 };
+
+export const getProfileByUserIdQueryDocument = graphql(`
+    query getProfileById($user_id: String!) {
+        getProfile(where: { user_id: $user_id }) {
+            minted
+            handle
+        }
+    }
+`);
 
 export const getProfileByUserIdQuery = async (user_id: string) => {
     const { getProfile } = await request(
@@ -76,94 +76,4 @@ export const getUserQuery = async (id: string) => {
     });
 
     return getUser;
-};
-
-export const createProfileQueryDocument = graphql(`
-    mutation CreateProfileMutation(
-        $handle: String!
-        $title: String!
-        $summary: String!
-        $job_type: String!
-        $pref_location: String!
-        $salary: String!
-        $years_of_exp: String!
-        $link: String!
-        $address: String!
-        $education: [JSON!]!
-        $experience: [JSON!]!
-        $user_id: String!
-    ) {
-        createOneProfile(
-            data: {
-                handle: $handle
-                title: $title
-                summary: $summary
-                job_type: $job_type
-                pref_location: $pref_location
-                salary: $salary
-                years_of_exp: $years_of_exp
-                link: $link
-                address: $address
-                education: { set: $education }
-                experience: { set: $experience }
-                minted: false
-                user: { connect: { id: $user_id } }
-            }
-        ) {
-            id
-            handle
-        }
-    }
-`);
-
-export const createReferralQueryDocument = graphql(`
-    mutation createOneReferral($email: String!, $user_id: String!) {
-        createOneReferral(
-            data: { email: $email, user: { connect: { id: $user_id } } }
-        ) {
-            id
-        }
-    }
-`);
-
-export const createReferralQuery = async (user_id: string, email: string) => {
-    const { createOneReferral } = await request(
-        API_URL,
-        createReferralQueryDocument,
-        {
-            email,
-            user_id,
-        }
-    ).catch((err) => {
-        console.log(err);
-        return { createOneReferral: null };
-    });
-
-    return createOneReferral;
-};
-
-export const createProfileQuery = async (user_id: string, profile: Profile) => {
-    const { createOneProfile } = await request(
-        API_URL,
-        createProfileQueryDocument,
-        {
-            handle: profile.handle,
-            title: profile.handle,
-            summary: profile.summary,
-            job_type: profile.job_type,
-            pref_location: profile.pref_location,
-            salary: profile.salary,
-            years_of_exp: profile.years_of_exp,
-            link: profile.link,
-            address: profile.address,
-            education: profile.education,
-            experience: profile.experience,
-            user_id: user_id,
-        }
-    ).catch((err) => {
-        console.log(err);
-        return { createOneProfile: null };
-    });
-
-    return createOneProfile;
 };
