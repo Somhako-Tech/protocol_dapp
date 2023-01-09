172509e6008ef75d90585dd9ed9b8cfc270371ec
kidesleo <kidus.dev@gmail.com>
2023-01-09 19:11:13 +0300

    Updated authorization configuration

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 import { Session } from "next-auth/core/types";
+
 import prisma from "../../../lib/prismadb";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import LinkedInProvider from "next-auth/providers/linkedin";
 
 export const authOptions = {
     adapter: PrismaAdapter(prisma),
@@ -11,28 +14,46 @@ export const authOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+        LinkedInProvider({
+            clientId: process.env.LINKEDIN_CLIENT_ID as string,
+            clientSecret: process.env.LINKEDIN_CLIENT_SECRET as string,
+        }),
         // ...add more providers here
     ],
     session: {
-        jwt: true,
+        strategy: "jwt",
         maxAge: 15 * 24 * 60 * 60, // the session will last 15 days
     },
     pages: {
         signIn: "/join",
     },
     callbacks: {
-        session: async ({ session, user }: { session: Session; user: any }) => {
+        session: async ({
+            session,
+            user,
+            token,
+        }: {
+            session: Session;
+            user: any;
+            token: any;
+        }) => {
             if (session?.user) {
-                session.user.id = user.id;
+                //Copy user id to session
+                session.user.id = token.sub;
             }
+            // console.log({ token });
             return session;
         },
+
         async redirect({ baseUrl }: { baseUrl: string }) {
             return baseUrl;
         },
         async signIn({ user }: { user: any }) {
             const isAllowedToSignIn = true;
-
             console.log("siginng in");
             if (isAllowedToSignIn) {
                 return true;
