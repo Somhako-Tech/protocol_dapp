941ba598ea20797463f1a28ce3fb8140f50689f3
Natnael <natnaelgetenew@gmail.com>
2023-01-23 10:16:28 +0300

    remove svg

--- pages/join/index.tsx
+++ pages/join/index.tsx
@@ -49,39 +49,9 @@ export default function LogIn({ providers }: { providers: any }) {
 
                             <div className="flex items-center mt-8 text-gray-600 dark:text-gray-400"></div>
 
-                            <div className="flex items-center mt-4 text-gray-600 dark:text-gray-400">
-                                <svg
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeLinejoin="round"
-                                    strokeWidth="1.5"
-                                    viewBox="0 0 24 24"
-                                    className="w-8 h-8 text-gray-500"
-                                >
-                                    <path
-                                        strokeLinejoin="round"
-                                        strokeWidth="1.5"
-                                        d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                                    />
-                                </svg>
-                            </div>
+                            <div className="flex items-center mt-4 text-gray-600 dark:text-gray-400"></div>
 
-                            <div className="flex items-center mt-2 text-gray-600 dark:text-gray-400">
-                                <svg
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeLinejoin="round"
-                                    strokeWidth="1.5"
-                                    viewBox="0 0 24 24"
-                                    className="w-8 h-8 text-gray-500"
-                                >
-                                    <path
-                                        strokeLinejoin="round"
-                                        strokeWidth="1.5"
-                                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                                    />
-                                </svg>
-                            </div>
+                            <div className="flex items-center mt-2 text-gray-600 dark:text-gray-400"></div>
                         </div>
                         <div className="container py-24 flex-col justify-center items-center text-white">
                             <h1 className="text-5xl font-bold text-center mb-6 text-white">
