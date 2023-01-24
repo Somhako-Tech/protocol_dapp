d83fcf510a5ed6408ec06c98ba28aa8cb15f049a
kidesleo <kidus.dev@gmail.com>
2023-01-24 16:27:54 +0300

    Added base documentation files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    async rewrites() {
+        return [{ source: "/docs", destination: "/html/docs.html" }];
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
