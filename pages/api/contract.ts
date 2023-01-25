a6cb8b56d3946ee0e0d21f1771defda51bd9e378
kidesleo <kidus.dev@gmail.com>
2023-01-25 16:18:39 +0300

    Added contract endpoint

---
+++ pages/api/contract.ts
@@ -0,0 +1,53 @@
+import { NextApiRequest } from "next";
+import ProfileManagerABI from "../../constants/protocol/ProfileManagerABI.json";
+import { ethers } from "ethers";
+import { getAddress } from "ethers/lib/utils";
+// This is an example of how to read a JSON Web Token from an API route
+// import { getToken } from "next-auth/jwt";
+
+export default async function handler(req: NextApiRequest, res: any) {
+    if (req.method !== "POST")
+        return res.status(405).send({ error: "Wrong method" });
+
+    const { address: owner, id, handle } = req.body.data;
+
+    console.log(`minting: ${JSON.stringify(req.body.data)}`);
+
+    const provider = new ethers.providers.JsonRpcProvider(
+        process.env.RPC_PROVIDER
+    );
+
+    const contractAddress =
+        process.env.CONTRACT_ADDRESS || "contract not found";
+
+    const signer = new ethers.Wallet(process.env.PRIVATE_KEY || "", provider);
+
+    const profileManagerContract = new ethers.Contract(
+        contractAddress,
+        ProfileManagerABI,
+        provider
+    );
+
+    const user_address = getAddress(owner);
+
+    const profileManagerWithSigner = profileManagerContract.connect(signer);
+
+    try {
+        const txResponse = await profileManagerWithSigner.safeMint({
+            owner: user_address,
+            id: id,
+            handle: handle,
+        });
+
+        await txResponse.wait();
+
+        const tokenId = await profileManagerWithSigner.balanceOf(owner);
+
+        res.setHeader("Access-Control-Allow-Origin", "*").send({
+            success: true,
+            tokenId,
+        });
+    } catch (error: any) {
+        res.send({ success: false, error });
+    }
+}
