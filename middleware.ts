10703390f46c0997e19ee69ce500cf54133fb80f
kidesleo <kidus.dev@gmail.com>
2023-01-25 16:17:56 +0300

    Updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-    matcher: ["/home", "/app", "/api/graphql"],
+    matcher: ["/home", "/app"],
     pages: {
         signIn: "/",
     },
