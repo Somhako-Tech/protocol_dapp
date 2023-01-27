98f1d7ce969f54c9dbf94ff572710d65e6ab4ab4
kidesleo <kidus.dev@gmail.com>
2023-01-27 16:15:21 +0300

    Refactored header

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,29 +8,14 @@ import { useProfileStore } from "../store";
 export default function Header({ handle }: { handle: string | null }) {
     const handleLink = `/u/${handle}`;
     return (
-        <section className="w-full flex flex-wrap px-4 items-center justify-center">
+        <section className="w-full flex flex-wrap px-4 items-center justify-center max-h-[200px]">
             <div className="w-full lg:max-w-80 ">
                 <div className="bg-white shadow-normal border-4 border-somhakohr2 rounded-[50px] p-6 center flex-col justify-center items-center">
                     <div className="flex justify-between items-center ">
-                        <div className="font-semibold text-lg">
+                        <div className="font-semibold text-lg  max-h-[200px]">
                             <Logo width={200} height={200} />
                         </div>
-                        <Link
-                            className="text-lg  mr-4 text-somhakohr font-medium rounded-full px-5 py-1.5 text-center "
-                            href={"/home"}
-                        >
-                            Home
-                        </Link>
-                        {handle && (
-                            <div>
-                                <Link
-                                    className="text-lg  mr-4 text-somhakohr font-medium rounded-full px-5 py-1.5 text-center "
-                                    href={handleLink}
-                                >
-                                    My Profile
-                                </Link>
-                            </div>
-                        )}
+
                         <div className="flex items-center justify-center">
                             <UserDropdownButton
                                 handleLink={handleLink}
