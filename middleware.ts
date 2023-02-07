af74fed809d218e2e3690c89cb59151b0fa89c11
Kidus Getachew <39452009+KidesLeo@users.noreply.github.com>
2023-02-07 20:05:26 +0300

    Added api in auth check

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-    matcher: ["/home", "/app", "/admin"],
+    matcher: ["/home", "/app", "/admin", "/api"],
     pages: {
         signIn: "/",
     },
