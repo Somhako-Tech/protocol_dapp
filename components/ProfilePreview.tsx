9c50f9b65a4394bc96a9e462ab76596a982d3da0
Kidus Getachew <39452009+KidesLeo@users.noreply.github.com>
2023-02-08 22:40:00 +0300

    Updated profile preview

--- components/ProfilePreview.tsx
+++ components/ProfilePreview.tsx
@@ -49,7 +49,7 @@ const ProfilePreview = ({ userProfile }: { userProfile: ProfileWithImage }) => {
 
     return (
         <div className="relative" key={userProfile.id}>
-            <div className="flex flex-col justify-evenly shadow-normal border shadow-slate-200 rounded-[30px] py-6 px-16 mb-6 text-center hover:shadow-2xl  h-[350px] absolute inset-0 z-10 opacity-0 hover:opacity-100 bg-white bg-opacity-98 duration-300">
+            <div className="flex flex-col justify-evenly shadow-normal border shadow-slate-200 rounded-[30px] py-6 pb-6 px-16 mb-6 text-center hover:shadow-2xl  h-[350px] absolute inset-0 z-10 opacity-0 hover:opacity-100 bg-white bg-opacity-98 duration-300">
                 <div className="my-2">
                     <label className="font-medium text-lg mb-2 leading-none inline-block">
                         Experience
@@ -67,7 +67,7 @@ const ProfilePreview = ({ userProfile }: { userProfile: ProfileWithImage }) => {
                     {"More >"}
                 </Link>
             </div>
-            <div className="flex flex-col justify-start bg-white shadow-normal border shadow-slate-200 rounded-[30px] py-6 px-16 mb-6 text-center hover:shadow-2xl h-[350px]">
+            <div className="flex flex-col justify-start bg-white shadow-normal border shadow-slate-200 rounded-[30px] py-6 pb-6 px-16 mb-6 text-center hover:shadow-2xl h-[350px]">
                 <div className="flex-col items-center justify-between">
                     <div className="flex justify-center mb-3 pb-2">
                         {userProfile.user.image ? (
@@ -91,15 +91,9 @@ const ProfilePreview = ({ userProfile }: { userProfile: ProfileWithImage }) => {
                         {userProfile.title}
                     </h2>
                     <div className="my-2" key="summary">
-                        <label
-                            htmlFor="summary"
-                            className={styles.profileLabel}
-                        >
-                            Summary
-                        </label>
                         <label id={"summary"} className={styles.profileText}>
                             {userProfile.summary.length > 120
-                                ? userProfile.summary.slice(0, 120) + "..."
+                                ? userProfile.summary.slice(0, 100) + "..."
                                 : userProfile.summary}
                         </label>
                     </div>
