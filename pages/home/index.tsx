951a58067d73952e3df33a58a2760b279b7f3231
Kidus Getachew <39452009+KidesLeo@users.noreply.github.com>
2023-02-09 20:51:07 +0300

    Updated home page

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -8,89 +8,62 @@ import { useQuery } from "@tanstack/react-query";
 import {
     getMintedProfilesQuery,
     getProfileByUserIdQuery,
-    getProfilesQuery,
-    getUserQuery,
 } from "../../graphql/graphqlQueries";
-import {
-    ProfileFormSkeleton,
-    ProfilePreviewSkeleton,
-} from "../../components/skeletons";
+import { ProfilePreviewSkeleton } from "../../components/skeletons";
 import ProfilePreview from "../../components/ProfilePreview";
 import Link from "next/link";
 import { Transition } from "react-transition-group";
+import { Profile } from "@prisma/client";
 
-export default function Home({
-    profileCreated = false,
-}: {
-    profileCreated: boolean;
-}) {
+export default function Home() {
     const router = useRouter();
     const { data: session } = useSession();
 
-    const {
-        isLoading: isProfileListLoading,
-        isError: profileListError,
-        data: Profiles,
-    } = useQuery(["getMintedProfiles"], async () => getMintedProfilesQuery(), {
-        cacheTime: 60,
-    });
+    const user = session?.user ? session.user : { id: null, email: null };
+
+    const [Profile, setProfile] = useState<Profile | null>(null);
+    const [isQueryLoading, setIsQueryLoading] = useState(true);
+
+    useEffect(() => {
+        const getProfile = async () => {
+            const profile = await getProfileByUserIdQuery(
+                (user.id as string) || "default"
+            );
+            profile && setProfile(profile);
+            setIsQueryLoading(false);
+        };
+        if (session) {
+            getProfile();
+        }
+    }, [session, user.id]);
+
+    const [profiles, setProfiles] = useState<Array<Profile> | null>(null);
+    const [isProfilesQueryLoading, setIsProfilesQueryLoading] = useState(true);
+
+    useEffect(() => {
+        const getProfiles = async () => {
+            const profiles = await getMintedProfilesQuery();
+            setIsProfilesQueryLoading(false);
+            setProfiles(profiles);
+        };
+
+        getProfiles();
+    }, []);
+    // const {
+    //     isLoading: isProfileListLoading,
+    //     isError: profileListError,
+    //     data: profiles,
+    // } = useQuery(["getMintedProfiles"], async () => getMintedProfilesQuery(), {
+    //     staleTime: 60,
+    // });
 
     useEffect(() => {
         if (!session) router.push("/");
     }, [session, router]);
 
-    if (isProfileListLoading) {
-        // if (true) {
-        return (
-            <section className="w-full flex flex-wrap ">
-                <div className="h-full w-full">
-                    <div className="w-full flex justify-center ">
-                        <Transition
-                            nodeRef={null}
-                            in={!profileCreated}
-                            timeout={400}
-                            className="my-10"
-                        >
-                            {(state) => (
-                                <Link
-                                    href="/app"
-                                    className={
-                                        state == "exited"
-                                            ? "opacity-0 h-0 transition-all 400ms"
-                                            : "opacity-100 transition-all 400ms my-2" +
-                                              "z-0 font-extrabold text-5xl text-center pt-5 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"
-                                    }
-                                >
-                                    Create Your Profile and Mint Now
-                                </Link>
-                            )}
-                        </Transition>
-                    </div>
-                    <div className="w-full mx-auto text-black	 bg-white shadow-normal  rounded-[25px] p-8 md:py-14 md:px-20">
-                        <div className="w-full mx-auto text-black	 bg-white shadow-normal  rounded-[25px] p-8 md:py-14 md:px-20">
-                            <h1
-                                className={
-                                    " font-bold text-4xl mb-4 text-center py-5"
-                                }
-                            >
-                                Minted Profiles
-                            </h1>
-
-                            <div className="grid grid-cols-4 grid-flow-row gap-4">
-                                {[...Array(5).keys()].map((i) => (
-                                    <ProfilePreviewSkeleton key={i} />
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        );
-    }
-
     let mintedProfileList = () => {
-        if (!isProfileListLoading && !profileListError && Profiles) {
-            const mintedProfileList = Profiles?.map((profile: any) => {
+        if (!isProfilesQueryLoading && profiles) {
+            const mintedProfileList = profiles?.map((profile: any) => {
                 return (
                     <ProfilePreview
                         userProfile={profile}
@@ -100,66 +73,45 @@ export default function Home({
             });
 
             return mintedProfileList;
-        } else return <></>;
+        } else
+            return [...Array(4).keys()].map((i) => (
+                <ProfilePreviewSkeleton key={i} />
+            ));
     };
 
     return (
-        <section className="w-full  flex flex-wrap ">
-            <div className=" h-full w-full">
-                <div className="w-full flex justify-center ">
-                    <Transition
-                        nodeRef={null}
-                        in={!profileCreated}
-                        timeout={400}
-                        className="my-10"
-                    >
-                        {(state) => (
-                            <Link
-                                href="/app"
-                                className={
-                                    state == "exited"
-                                        ? "opacity-0 h-0 transition-all 400ms"
-                                        : "opacity-100 transition-all 400ms my-2" +
-                                          "z-0 font-extrabold text-5xl text-center pt-5 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"
-                                }
-                            >
-                                Create Your Profile and Mint Now
-                            </Link>
-                        )}
-                    </Transition>
-                </div>
+        <section className="mx-10 flex flex-wrap">
+            <div className="flex w-full justify-center">
+                <Transition
+                    nodeRef={null}
+                    in={!Profile}
+                    timeout={400}
+                    className="my-10"
+                >
+                    {(state) => (
+                        <Link
+                            href="/app"
+                            className={
+                                state == "exited"
+                                    ? "400ms h-0 opacity-0 transition-all"
+                                    : "400ms my-2 opacity-100 transition-all" +
+                                      "z-0 bg-gradient-to-r from-purple-200 to-pink-300 bg-clip-text py-8 pt-5 text-center text-5xl font-extrabold text-transparent"
+                            }
+                        >
+                            Create Your Profile and Mint Now
+                        </Link>
+                    )}
+                </Transition>
+            </div>
 
-                <div className="w-full mx-auto text-black	 bg-white shadow-normal  rounded-[25px] p-8 md:py-14 md:px-20">
-                    <h1 className={" font-bold text-4xl mb-4 text-center py-5"}>
-                        Minted Profiles
-                    </h1>
-                    <div className="grid grid-cols-4 grid-flow-row gap-4">
-                        {mintedProfileList()}
-                    </div>
+            <div className="shadow-normal mx-auto w-full rounded-[25px] bg-white bg-gradient-to-r from-slate-50 to-slate-200 p-8 text-black md:py-14 md:px-20">
+                <h1 className={" mb-4 py-5 text-center text-4xl font-bold"}>
+                    Web3 Profiles
+                </h1>
+                <div className="grid grid-flow-row grid-cols-4 gap-4 ">
+                    {mintedProfileList()}
                 </div>
             </div>
         </section>
     );
-}
-
-import { authOptions } from "../api/auth/[...nextauth]";
-import { unstable_getServerSession } from "next-auth/next";
-
-export async function getServerSideProps(context: any) {
-    //TODO Fix this type
-    const session: any = await unstable_getServerSession(
-        context.req,
-        context.res,
-        authOptions
-    );
-
-    const user = session?.user ? session.user : { id: "default" };
-
-    const userProfile = await getProfileByUserIdQuery(user.id as string);
-
-    return {
-        props: {
-            profileCreated: userProfile !== null,
-        },
-    };
 }
