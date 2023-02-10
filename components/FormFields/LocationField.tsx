bb188b25c21ade42fd8eb0c79dcf173f5621e369
Kidus Getachew <39452009+KidesLeo@users.noreply.github.com>
2023-02-10 13:44:02 +0300

    Refactored location field

--- components/FormFields/LocationField.tsx
+++ components/FormFields/LocationField.tsx
@@ -1,8 +1,9 @@
 import { Profile } from "@prisma/client";
 import { Combobox } from "@headlessui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import SearchComboBox from "./SearchComboBox";
+import { ClipLoader } from "react-spinners";
 
 export default function LocationField({
     userProfile,
@@ -11,18 +12,23 @@ export default function LocationField({
     userProfile: Profile;
     handleChange: (e: any) => void;
 }) {
-    const [selectedLocation, setSelectedLocation] = useState("");
+    // const [selectedLocation, setSelectedLocation] = useState("");
     const [searchParams, setSearchParams] = useState("");
+    const [locations, setLocations] = useState([]);
+    const [isQueryLoading, setIsQueryLoading] = useState(false);
 
-    const {
-        data: Locations,
-        isLoading: queryLoading,
-        isError: queryError,
-    } = useQuery(["getLocations", searchParams], () =>
-        fetch(`/api/search/locations?search=${searchParams}`).then((data) =>
-            data.json().then((j) => j.locations)
-        )
-    );
+    useEffect(() => {
+        const getLocations = async () => {
+            const newLocations = await fetch(
+                `/api/search/locations?search=${searchParams}`
+            ).then((data) => data.json().then((j) => j.locations));
+            setIsQueryLoading(false);
+            setLocations(newLocations);
+        };
+
+        setIsQueryLoading(true);
+        getLocations();
+    }, [searchParams]);
 
     return (
         <div className="formInputPair">
@@ -33,19 +39,26 @@ export default function LocationField({
                 Preferred Location
             </label>
 
-            <SearchComboBox
-                handleChange={(item) =>
-                    handleChange({
-                        target: {
-                            id: "pref_location",
-                            value: item,
-                        },
-                    })
-                }
-                setSearchParams={setSearchParams}
-                data={queryLoading ? [] : Locations}
-                value={userProfile.pref_location}
-            />
+            <div className="relative flex flex-row">
+                <SearchComboBox
+                    handleChange={(item) =>
+                        handleChange({
+                            target: {
+                                id: "pref_location",
+                                value: item,
+                            },
+                        })
+                    }
+                    setSearchParams={setSearchParams}
+                    data={isQueryLoading ? [] : locations}
+                    value={userProfile.pref_location}
+                />
+                {isQueryLoading && (
+                    <div className="absolute right-8 top-2">
+                        <ClipLoader size={20} />
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
