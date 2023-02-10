4b85073200694644c713a6a615ebb0858a221dc9
Kidus Getachew <39452009+KidesLeo@users.noreply.github.com>
2023-02-10 13:51:57 +0300

    Added exclude and an alert to skill search

--- components/FormFields/SearchComboBoxMultiple.tsx
+++ components/FormFields/SearchComboBoxMultiple.tsx
@@ -6,10 +6,12 @@ export default function SearchComboBox({
     searchParams,
     handleChange,
     data,
+    exclude,
 }: {
     setSearchParams: (value: string) => void;
     handleChange: (e: any) => void;
     data: Array<string> | undefined;
+    exclude: Array<string>;
     searchParams: string;
 }) {
     const [open, setOpen] = useState(false);
@@ -29,20 +31,22 @@ export default function SearchComboBox({
                 {open && (
                     <Combobox.Options className="absolute mt-3 w-full rounded-md border bg-white shadow-md">
                         {data &&
-                            data.map((item: string) => (
-                                <Combobox.Option
-                                    key={item}
-                                    value={item}
-                                    className="w-full rounded-md p-1 px-2 hover:bg-indigo-500 hover:text-white"
-                                    onClick={() => {
-                                        handleChange(item);
-                                        setOpen(false);
-                                        setSearchParams("");
-                                    }}
-                                >
-                                    {item}
-                                </Combobox.Option>
-                            ))}
+                            data
+                                .filter((item) => !exclude.includes(item))
+                                .map((item: string) => (
+                                    <Combobox.Option
+                                        key={item}
+                                        value={item}
+                                        className="w-full rounded-md p-1 px-2 hover:bg-indigo-500 hover:text-white"
+                                        onClick={() => {
+                                            handleChange(item);
+                                            setOpen(false);
+                                            setSearchParams("");
+                                        }}
+                                    >
+                                        {item}
+                                    </Combobox.Option>
+                                ))}
                     </Combobox.Options>
                 )}
             </Combobox>
--- components/FormFields/SkillField.tsx
+++ components/FormFields/SkillField.tsx
@@ -3,6 +3,8 @@ import { Combobox } from "@headlessui/react";
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import SearchComboBox from "./SearchComboBoxMultiple";
+import Alert from "@mui/material/Alert/Alert";
+import { Transition } from "react-transition-group";
 
 export default function SkillFields({
     userProfile,
@@ -53,9 +55,10 @@ export default function SkillFields({
                     setSearchParams={setSearchParams}
                     data={queryLoading ? [] : Skills}
                     searchParams={searchParams}
+                    exclude={userProfile.skills}
                 />
             </div>
-            <div className="border-slate- my-2 flex items-center justify-start self-center border py-2  capitalize text-white">
+            <div className="my-2 flex items-center justify-start self-center border py-2  capitalize text-white">
                 {userProfile.skills &&
                     (userProfile.skills.length > 0 ? (
                         <div className="grid max-w-md grid-cols-3 border-4 border-slate-500">
@@ -74,14 +77,26 @@ export default function SkillFields({
                             )}
                         </div>
                     ) : (
-                        <div className="flex max-w-md flex-col border-4 border-slate-700">
-                            <label
-                                className="m-2 rounded-md bg-somhakohr px-4 py-1 hover:bg-somhakohr2"
-                                htmlFor="searchInput"
-                            >
-                                Add Skills
-                            </label>
-                        </div>
+                        <Transition
+                            nodeRef={null}
+                            in={userProfile.skills.length === 0}
+                            timeout={400}
+                        >
+                            {(state) => (
+                                <div ref={null}>
+                                    <Alert
+                                        severity="info"
+                                        className={
+                                            state == "exited"
+                                                ? "400ms h-0 opacity-0 transition-all"
+                                                : "400ms opacity-100 transition-all"
+                                        }
+                                    >
+                                        Add skills!
+                                    </Alert>
+                                </div>
+                            )}
+                        </Transition>
                     ))}
             </div>
         </div>
