db5fa48c05f9599abb8bb1a3cdc88d05a3b46578
Kidus Getachew <39452009+KidesLeo@users.noreply.github.com>
2023-02-10 13:43:43 +0300

    Updated location endpoint to resolve faster

--- pages/api/search/locations.ts
+++ pages/api/search/locations.ts
@@ -10,20 +10,21 @@ async function getLocations(search: string) {
     const result: Array<string> = [];
 
     return new Promise(function (resolve, reject) {
+        let count = 0;
         fs.createReadStream(datasetPath + "/worldcities.csv")
             .pipe(parse({ delimiter: ",", from_line: 2 }))
             .on("data", function (row) {
+                if (count > 5) return;
                 if (row[0].search(pattern) != -1) {
                     result.push(`${row[0]}, ${row[4]}`);
+                    count += 1;
                 }
             })
             .on("end", () => {
-                result
-                    .sort(
-                        (item_a, item_b) =>
-                            item_a.search(pattern) - item_b.search(pattern)
-                    )
-                    .slice(0, 6);
+                result.sort(
+                    (item_a, item_b) =>
+                        item_a.search(pattern) - item_b.search(pattern)
+                );
                 resolve(result);
             })
             .on("error", reject);
