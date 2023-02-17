5018b0e8b1efb1417e3267970d9ae65e661bb31e
Kidus Getachew <39452009+KidesLeo@users.noreply.github.com>
2023-02-17 13:46:54 +0300

    Refactored contract

--- pages/api/contract.ts
+++ pages/api/contract.ts
@@ -2,6 +2,7 @@ import { NextApiRequest } from "next";
 import ProfileManagerABI from "../../constants/protocol/ProfileManagerABI.json";
 import { ethers } from "ethers";
 import { getAddress } from "ethers/lib/utils";
+import { Profile } from "@prisma/client";
 // This is an example of how to read a JSON Web Token from an API route
 // import { getToken } from "next-auth/jwt";
 
@@ -13,18 +14,7 @@ export default async function handler(req: NextApiRequest, res: any) {
         process.env.RPC_PROVIDER as string
     );
 
-    const {
-        address: owner,
-        user_id,
-        handle,
-        ipfs_hash,
-        education,
-        experience,
-    } = req.body;
-
-    console.log(
-        `minting: ${JSON.stringify(req.body)} @ ${process.env.RPC_PROVIDER}`
-    );
+    const profile: Profile = req.body;
 
     const contractAddress = process.env.CONTRACT_ADDRESS as string;
 
@@ -40,28 +30,25 @@ export default async function handler(req: NextApiRequest, res: any) {
             provider
         );
 
-        const user_address = getAddress(owner);
+        const user_address = getAddress(profile.address);
 
         const profileManagerWithSigner = profileManagerContract.connect(signer);
 
         const txReceipt = await profileManagerWithSigner.safeMint(
             {
                 owner: user_address,
-                id: user_id,
-                handle: handle,
-                image_location_hash: ipfs_hash,
+                id: profile.user_id,
+                handle: profile.handle,
+                image_location_hash: profile.ipfs_hash,
             },
-            education,
-            experience
+            profile.education,
+            profile.experience
         );
 
         await txReceipt.wait();
 
-        const tokenId = await profileManagerWithSigner.balanceOf(owner);
-
         res.setHeader("Access-Control-Allow-Origin", "*").send({
             success: true,
-            tokenId,
             txReceipt,
         });
     } catch (error: any) {
