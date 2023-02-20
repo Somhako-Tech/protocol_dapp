1ede1b7e21fa0eaea4014b02c11cb0e53af000b2
Kidus Getachew <39452009+KidesLeo@users.noreply.github.com>
2023-02-20 17:27:47 +0300

    Updated profile preview component

--- components/ProfilePreview.tsx
+++ components/ProfilePreview.tsx
@@ -42,9 +42,61 @@ const ProfilePreview = ({ userProfile }: { userProfile: ProfileWithImage }) => {
         )
     );
 
+    const skills = userProfile.skills.map((skill) => (
+        <li
+            className="my-2 mr-3 min-w-[80px] rounded-full bg-gray-200 p-1 px-2 text-center text-sm text-black"
+            key={skill}
+        >
+            {skill}
+        </li>
+    ));
+
+    return (
+        <div
+            className="h-full rounded-lg border p-5 shadow-md"
+            key={userProfile.user_id}
+        >
+            <div className="mb-3 flex flex-wrap items-center">
+                <div className="mb-6 w-full sm:mb-0 sm:max-w-[65px]">
+                    <Image
+                        src={
+                            "https://gateway.pinata.cloud/ipfs/" +
+                            userProfile.ipfs_hash
+                        }
+                        alt="@Sam"
+                        width={65}
+                        height={65}
+                        className="rounded-full"
+                    />
+                </div>
+                <div className="w-full sm:max-w-[calc(100%-65px)] sm:pl-6">
+                    <h1 className="mb-2 font-semibold">
+                        @{userProfile.handle}
+                    </h1>
+                    <h5 className="font-medium text-darkGray">
+                        {userProfile.title}
+                    </h5>
+                </div>
+            </div>
+            <ul className="mb-3 flex flex-wrap items-center uppercase">
+                {skills}
+            </ul>
+
+            <p className="my-2 mb-6 text-sm text-darkGray">
+                Expectation - {userProfile.salary}
+            </p>
+            <Link
+                href={"/u/" + userProfile.handle}
+                className="inline-block rounded bg-gradient-to-r from-secondary to-tertiary py-2 px-8 text-sm font-bold text-white transition-all hover:from-primary hover:to-primary"
+            >
+                View
+            </Link>
+        </div>
+    );
+
     return (
         <div className="relative" key={userProfile.user_id}>
-            <div className="shadow-normal bg-opacity-98 absolute inset-0 z-10 mb-6 flex min-h-[350px] flex-col justify-evenly rounded-[30px] border bg-white  py-6 px-16 pb-6 text-center opacity-0 shadow-slate-200 duration-300 hover:opacity-100 hover:shadow-2xl">
+            <div className="bg-opacity-98 absolute inset-0 z-10 mb-6 flex min-h-[350px] flex-col justify-evenly rounded-[30px] border bg-white py-6  px-16 pb-6 text-center opacity-0 shadow-normal shadow-slate-200 duration-300 hover:opacity-100 hover:shadow-2xl">
                 <div className="my-2">
                     <label className="mb-2 inline-block text-lg font-medium leading-none">
                         Experience
@@ -57,12 +109,12 @@ const ProfilePreview = ({ userProfile }: { userProfile: ProfileWithImage }) => {
                 <div className="my-2">{education}</div>
                 <Link
                     href={`/u/${userProfile.handle}`}
-                    className="font-normal text-somhako"
+                    className="text-somhako font-normal"
                 >
                     {"More >"}
                 </Link>
             </div>
-            <div className="shadow-normal mb-6 flex min-h-[350px] flex-col justify-start rounded-[30px] border bg-white py-6 px-16 pb-6 text-center shadow-slate-200 hover:shadow-2xl">
+            <div className="mb-6 flex min-h-[350px] flex-col justify-start rounded-[30px] border bg-white py-6 px-16 pb-6 text-center shadow-normal shadow-slate-200 hover:shadow-2xl">
                 <div className="flex-col items-center justify-between">
                     <div className="mb-3 flex justify-center pb-2">
                         {userProfile.user.image ? (
