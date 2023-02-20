e0fadeb77edb520338171390784b17a5850c3cb6
Kidus Getachew <39452009+KidesLeo@users.noreply.github.com>
2023-02-20 17:30:08 +0300

    Added default value for experience click

--- components/FormFields/ExperienceFields.tsx
+++ components/FormFields/ExperienceFields.tsx
@@ -59,6 +59,7 @@ export default function ExperienceFields({
                 onClick={onClick}
                 ref={ref}
                 value={value || ""}
+                defaultValue={""}
             />
         ));
 
