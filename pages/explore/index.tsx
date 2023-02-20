7992ac4fe39169fdb9b1c8370f1c8278d679f5ed
Kidus Getachew <39452009+KidesLeo@users.noreply.github.com>
2023-02-20 17:29:41 +0300

    Updated explore page

--- pages/explore/index.tsx
+++ pages/explore/index.tsx
@@ -14,7 +14,8 @@ import Link from "next/link";
 import { Transition } from "react-transition-group";
 import { Profile } from "@prisma/client";
 import Image from "next/image";
-import userImage from '/public/images/userImage.png'
+import userImage from "/public/images/userImage.png";
+import { BigClipLoader } from "../../components/Loader";
 
 export default function Explore() {
     const router = useRouter();
@@ -71,176 +72,41 @@ export default function Explore() {
             });
 
             return mintedProfileList;
-        } else
-            return [...Array(4).keys()].map((i) => (
-                <ProfilePreviewSkeleton key={i} />
-            ));
+        } else return <BigClipLoader color="purple" />;
+        /* return [...Array(4).keys()].map((i) => (
+            <ProfilePreviewSkeleton key={i} />
+        )); */
     };
 
     return (
         <section className="py-10">
             <div className="container">
-                <div className="shadow-normal rounded-normal p-4 md:p-10 bg-white">
+                <div className="rounded-normal bg-white p-4 shadow-normal md:p-10">
                     <div className="mb-4 flex flex-wrap items-center">
-                        <h1 className="mr-3 font-bold text-2xl">
-                            {
-                            !Profile
-                            ? <>Create Your Profile and <span className="text-primary">Mint</span> Now</>
-                            : <>You're In the <span className="text-primary">Mint</span> Queue</>
-                            }
+                        <h1 className="mr-3 text-2xl font-bold">
+                            {!Profile ? (
+                                <>
+                                    Create Your Profile and{" "}
+                                    <span className="text-primary">Mint</span>{" "}
+                                    Now
+                                </>
+                            ) : (
+                                <>
+                                    {"You're In the"}{" "}
+                                    <span className="text-primary">Mint</span>{" "}
+                                    Queue
+                                </>
+                            )}
                         </h1>
                         <Link
                             href={!Profile ? "/app" : "/u"}
-                            className="inline-block my-3 bg-gradient-to-r from-secondary to-tertiary text-white text-sm font-bold rounded py-2 px-6 transition-all hover:from-primary hover:to-primary"
+                            className="my-3 inline-block rounded bg-gradient-to-r from-secondary to-tertiary py-2 px-6 text-sm font-bold text-white transition-all hover:from-primary hover:to-primary"
                         >
-                            {
-                            !Profile
-                            ? "Mint Now"
-                            : "Mint Queue"
-                            }
-                        </Link>
-
-                    </div>
-                    <p className="font-semibold mb-4 font-bold">Web 3 Profiles</p>
-                    <div className="flex flex-wrap mx-[-15px]">
-                        <div className="w-full md:max-w-[50%] xl:max-w-[calc(100%/3)] mb-6 px-[15px]">
-                            <div className="border rounded-lg shadow-md p-5 h-full">    
-                                <div className="flex flex-wrap items-center mb-3">
-                                    <div className="w-full sm:max-w-[65px] mb-6 sm:mb-0">
-                                        <Image src={userImage} alt="@Sam" width={65} height={65} className="rounded-full" />
-                                    </div>
-                                    <div className="w-full sm:max-w-[calc(100%-65px)] sm:pl-6">
-                                        <h1 className="font-semibold mb-2">@SAM</h1>
-                                        <h5 className="text-darkGray font-medium">Product Manager</h5>
-                                    </div>
-                                </div>
-                                <ul className="flex flex-wrap items-center uppercase mb-3">
-                                    <li className="bg-gray-200 text-sm text-center min-w-[80px] rounded-full text-black p-1 px-2 my-2 mr-3">
-                                        HTML
-                                    </li>
-                                    <li className="bg-gray-200 text-sm text-center min-w-[80px] rounded-full text-black p-1 px-2 my-2 mr-3">
-                                        php
-                                    </li>
-                                    <li className="bg-gray-200 text-sm text-center min-w-[80px] rounded-full text-black p-1 px-2 my-2 mr-3">
-                                        Java
-                                    </li>
-                                </ul>
-                                <p className="text-darkGray text-sm my-2">Notice Period - 30 Days</p>
-                                <p className="text-darkGray text-sm my-2 mb-6">Expectation - 7.5 LPA</p>
-                                <Link href={'#'} className="inline-block bg-gradient-to-r from-secondary to-tertiary text-white text-sm font-bold rounded py-2 px-8 transition-all hover:from-primary hover:to-primary">View</Link>
-                            </div>
-                        </div>
-                        <div className="w-full md:max-w-[50%] xl:max-w-[calc(100%/3)] mb-6 px-[15px]">
-                            <div className="border rounded-lg shadow-md p-5 h-full">    
-                                <div className="flex flex-wrap items-center mb-3">
-                                    <div className="w-full sm:max-w-[65px] mb-6 sm:mb-0">
-                                        <Image src={userImage} alt="@Sam" width={65} height={65} className="rounded-full" />
-                                    </div>
-                                    <div className="w-full sm:max-w-[calc(100%-65px)] sm:pl-6">
-                                        <h1 className="font-semibold mb-2">@SAM</h1>
-                                        <h5 className="text-darkGray font-medium">Product Manager</h5>
-                                    </div>
-                                </div>
-                                <ul className="flex flex-wrap items-center uppercase mb-3">
-                                    <li className="bg-gray-200 text-sm text-center min-w-[80px] rounded-full text-black p-1 px-2 my-2 mr-3">
-                                        HTML
-                                    </li>
-                                    <li className="bg-gray-200 text-sm text-center min-w-[80px] rounded-full text-black p-1 px-2 my-2 mr-3">
-                                        php
-                                    </li>
-                                    <li className="bg-gray-200 text-sm text-center min-w-[80px] rounded-full text-black p-1 px-2 my-2 mr-3">
-                                        Java
-                                    </li>
-                                </ul>
-                                <p className="text-darkGray text-sm my-2">Notice Period - 30 Days</p>
-                                <p className="text-darkGray text-sm my-2 mb-6">Expectation - 7.5 LPA</p>
-                                <Link href={'#'} className="inline-block bg-gradient-to-r from-secondary to-tertiary text-white text-sm font-bold rounded py-2 px-8 transition-all hover:from-primary hover:to-primary">View</Link>
-                            </div>
-                        </div>
-                        <div className="w-full md:max-w-[50%] xl:max-w-[calc(100%/3)] mb-6 px-[15px]">
-                            <div className="border rounded-lg shadow-md p-5 h-full">    
-                                <div className="flex flex-wrap items-center mb-3">
-                                    <div className="w-full sm:max-w-[65px] mb-6 sm:mb-0">
-                                        <Image src={userImage} alt="@Sam" width={65} height={65} className="rounded-full" />
-                                    </div>
-                                    <div className="w-full sm:max-w-[calc(100%-65px)] sm:pl-6">
-                                        <h1 className="font-semibold mb-2">@SAM</h1>
-                                        <h5 className="text-darkGray font-medium">Product Manager</h5>
-                                    </div>
-                                </div>
-                                <ul className="flex flex-wrap items-center uppercase mb-3">
-                                    <li className="bg-gray-200 text-sm text-center min-w-[80px] rounded-full text-black p-1 px-2 my-2 mr-3">
-                                        HTML
-                                    </li>
-                                    <li className="bg-gray-200 text-sm text-center min-w-[80px] rounded-full text-black p-1 px-2 my-2 mr-3">
-                                        php
-                                    </li>
-                                    <li className="bg-gray-200 text-sm text-center min-w-[80px] rounded-full text-black p-1 px-2 my-2 mr-3">
-                                        Java
-                                    </li>
-                                </ul>
-                                <p className="text-darkGray text-sm my-2">Notice Period - 30 Days</p>
-                                <p className="text-darkGray text-sm my-2 mb-6">Expectation - 7.5 LPA</p>
-                                <Link href={'#'} className="inline-block bg-gradient-to-r from-secondary to-tertiary text-white text-sm font-bold rounded py-2 px-8 transition-all hover:from-primary hover:to-primary">View</Link>
-                            </div>
-                        </div>
-                        <div className="w-full md:max-w-[50%] xl:max-w-[calc(100%/3)] mb-6 px-[15px]">
-                            <div className="border rounded-lg shadow-md p-5 h-full">    
-                                <div className="flex flex-wrap items-center mb-3">
-                                    <div className="w-full sm:max-w-[65px] mb-6 sm:mb-0">
-                                        <Image src={userImage} alt="@Sam" width={65} height={65} className="rounded-full" />
-                                    </div>
-                                    <div className="w-full sm:max-w-[calc(100%-65px)] sm:pl-6">
-                                        <h1 className="font-semibold mb-2">@SAM</h1>
-                                        <h5 className="text-darkGray font-medium">Product Manager</h5>
-                                    </div>
-                                </div>
-                                <ul className="flex flex-wrap items-center uppercase mb-3">
-                                    <li className="bg-gray-200 text-sm text-center min-w-[80px] rounded-full text-black p-1 px-2 my-2 mr-3">
-                                        HTML
-                                    </li>
-                                    <li className="bg-gray-200 text-sm text-center min-w-[80px] rounded-full text-black p-1 px-2 my-2 mr-3">
-                                        php
-                                    </li>
-                                    <li className="bg-gray-200 text-sm text-center min-w-[80px] rounded-full text-black p-1 px-2 my-2 mr-3">
-                                        Java
-                                    </li>
-                                </ul>
-                                <p className="text-darkGray text-sm my-2">Notice Period - 30 Days</p>
-                                <p className="text-darkGray text-sm my-2 mb-6">Expectation - 7.5 LPA</p>
-                                <Link href={'#'} className="inline-block bg-gradient-to-r from-secondary to-tertiary text-white text-sm font-bold rounded py-2 px-8 transition-all hover:from-primary hover:to-primary">View</Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex w-full justify-center ">
-                    <div
-                        className={
-                            !isQueryLoading
-                                ? "m-10 rounded-lg border-2 border-slate-400 py-4 px-4 transition-all hover:scale-105 hover:border-0 hover:bg-purple-300 hover:bg-gradient-to-r hover:from-slate-50 hover:to-slate-200 hover:shadow-md hover:shadow-white"
-                                : " m-10 py-4 px-4 transition-all"
-                        }
-                    >
-                        <Link
-                            href={!Profile ? "/app" : "/u"}
-                            className={
-                                isQueryLoading
-                                    ? "400ms h-0 opacity-0 transition-all"
-                                    : "400ms my-2 opacity-100 transition-all" +
-                                    " z-0 bg-gradient-to-r from-purple-200 to-pink-300 bg-clip-text text-5xl font-extrabold text-transparent hover:bg-gradient-to-r hover:from-amber-600 hover:to-yellow-900"
-                            }
-                        >
-                            {!Profile
-                                ? "Create Your Profile and Mint Now"
-                                : "You're In the Mint Queue"}
+                            {!Profile ? "Mint Now" : "Mint Queue"}
                         </Link>
                     </div>
-                </div>
-                <div className="shadow-normal mx-auto w-full rounded-[25px] bg-gradient-to-r from-slate-50 to-slate-200 p-8 text-black md:py-14 md:px-20">
-                    <h1 className={" mb-4 py-5 text-center text-4xl font-bold"}>
-                        Web3 Profiles
-                    </h1>
-                    <div className="grid grid-flow-row grid-cols-4 gap-4 ">
+                    <p className="mb-4 font-bold">Web 3 Profiles</p>
+                    <div className="mx-[-15px] flex flex-wrap">
                         {mintedProfileList()}
                     </div>
                 </div>
