d593e19b0f95ecb3f051594d47a131854fd23eb4
Kidus Getachew <39452009+KidesLeo@users.noreply.github.com>
2023-02-22 10:10:52 +0300

    Updated mail endpoint

--- pages/api/mail/index.ts
+++ pages/api/mail/index.ts
@@ -1,37 +1,59 @@
 import { NextApiRequest } from "next";
 import path from "path";
 import { promises as fs } from "fs";
-import * as jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import emails from "../../../constants/email";
 
 export default async function handler(req: NextApiRequest, res: any) {
     if (req.method !== "POST")
         return res.status(405).send("Failed to authenticate");
 
     //TODO Finish auth
-    // const secret = process.env.NEXTAUTH_SECRET || "";
+    const secret = process.env.NEXTAUTH_SECRET || "";
 
-    // const token = req.headers.authorization?.split(" ")[1];
+    const token = req.headers["protocol-token"];
 
-    // const resp = jwt.verify(token as string, secret, (error, decodedToken) => {
-    //     if (error || !decodedToken) {
-    //         console.log({ error });
-    //         return res.status(401).json({ error: "not authorized" });
-    //     } else return res.status(200).json({ message: "success" });
-    // });
+    const tokenDec = jwt.verify(
+        token as string,
+        secret,
+        (error, decodedToken) => {
+            if (error || !decodedToken) {
+                console.log({ error });
+                return null;
+            }
+            return decodedToken;
+        }
+    );
 
+    if (!token) return res.json({ error: "Authentication failed" });
     const sgMail = require("@sendgrid/mail");
 
     const { to, subject, html } = req.body;
 
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const msg = {
-        to: to,
-        from: process.env.DEFAULT_FROM_EMAIL,
-        subject: subject,
-        html: html,
-    };
-    const resp = await sgMail.send(msg);
 
-    if (resp) res.status(200).json({ success: true });
-    else res.status(500);
+    if (!subject && !html) {
+        const msg = {
+            to: to,
+            from: process.env.DEFAULT_FROM_EMAIL,
+            subject: emails.profileCreated.subject,
+            html: emails.profileCreated.html,
+        };
+        const resp = await sgMail.send(msg);
+
+        if (resp) res.status(200).json({ success: true });
+        else res.status(500);
+    } else {
+        const msg = {
+            to: to,
+            from: process.env.DEFAULT_FROM_EMAIL,
+            subject: subject,
+            html: html,
+        };
+
+        const resp = await sgMail.send(msg);
+
+        if (resp) res.status(200).json({ success: true });
+        else res.status(500);
+    }
 }
